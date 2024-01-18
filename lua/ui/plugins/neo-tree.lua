b42f405f6a55c082bad8474bcc8299c1df510368
Will Robertson <w.robertson1229@gmail.com>
2024-01-18 14:39:12 -0600

    Added and changed Neotree keybindings based on recommendations

--- lua/ui/plugins/neo-tree.lua
+++ lua/ui/plugins/neo-tree.lua
@@ -13,9 +13,21 @@ return {
     require('neo-tree').setup({
       view = { adaptive_size = true }
     })
-    vim.keymap.set('n', '<C-b>', function()
-      vim.cmd("Neotree toggle")
+    vim.keymap.set('n', '<C-_>', function() -- <C-_> is the alias for the 0x1F command code, which is returned by Ctrl+/
+      vim.cmd("Neotree toggle current reveal_force_cwd")
     end, { desc = "Toggle [N]eo-tree" })
+    vim.keymap.set('n', '|', function()
+      vim.cmd("Neotree reveal")
+    end, { desc = "Reveal Neo-tree" })
+    vim.keymap.set('n', '<leader>N', function()
+      vim.cmd("Neotree float reveal_file=<cfile> reveal_force_cwd")
+    end, { desc = "Goto [N]eotree Directory" })
+    vim.keymap.set('n', '<leader>b', function()
+      vim.cmd("Neotree toggle show buffers right")
+    end, { desc = "Toggle Show [B]uffers" })
+    vim.keymap.set('n', '<leader>gs', function()
+      vim.cmd("Neotree float git_status")
+    end, { desc = "Show Git [S]tatus" })
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
